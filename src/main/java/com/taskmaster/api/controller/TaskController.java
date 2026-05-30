36426eb317b591f1b2a27619312ec8c9ea24d1bd
Navod Yasara <navodyasara567@gmail.com>
2026-05-30 20:55:01 +0530

    feat: implement task CRUD and JWT authenication - add TaskService Update and Delete logic with security ownership checks - Expose TaskController endpoints for updating & deleting tasks.

--- src/main/java/com/taskmaster/api/controller/TaskController.java
+++ src/main/java/com/taskmaster/api/controller/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,11 @@ public class TaskController {
     public ResponseEntity<UpdateTaskDTO> updateTask(@PathVariable Long taskId, @RequestBody UpdateTaskDTO updateRequest){
         UpdateTaskDTO response = taskService.UpdateTask(taskId, updateRequest);
         return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/deleteTask/{taskId")
+    public ResponseEntity deleteTask(@PathVariable Long taskId){
+        taskService.deleteTask(taskId);
+        return ResponseEntity.ok("Success");
     }
 }
--- src/main/java/com/taskmaster/api/service/TaskService.java
+++ src/main/java/com/taskmaster/api/service/TaskService.java
@@ -21,126 +21,145 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class TaskService {
 
-    /*
-     * JAVA වලදී, int, boolean වගේ පොඩි දේවල් (Primitive types) කෙලින්ම variable
-     * එකක් ඇතුලේ තියාගන්න පුළුවන්
-     * වුණාට, TaskRepository වගේ ලොකු දේවල් (Objects) කෙලින්ම ඒක අස්සේ ඔබලා තියන්න
-     * බැහැ. ඒ වෙනුවට
-     * මේ variables කරන්නේ අර හැදුණු ඔරිජිනල් ගබඩාව තියෙන්නේ කොහෙද කියලා පෙන්වන එකයි
-     * (Refer කරන එකයි).
-     */
-    private final TaskRepository taskRepository;
-    private final UserRepository userRepository;
-
-    // constructor (Dependency injection)
-    /*
-     * හිතන්න අපේ TaskService කියන්නේ ආපනශාලාවක ඉන්න ප්‍රධාන කෝකියා (Head Chef)
-     * කියලා.
-     * එයාගේ ප්‍රධාන වැඩේ තමයි ඔර්ඩර්ස් (Tasks) කළමනාකරණය කරන එක.
-     * හැබැයි කෝකියාට මේ වැඩේ තනියම කරන්න බැහැ. එයාට උදව්වට ප්‍රධාන දේවල් දෙකක් ඕනේ
-     * වෙනවා:
-     * TaskRepository - කෑම බඩු තියෙන ප්‍රධාන ගබඩාව (Database එකේ Tasks තියාගන්න
-     * තැන).
-     * UserRepository - පාරිභෝගිකයින්ගේ විස්තර තියෙන පොත (Database එකේ Users ලා ඉන්න
-     * තැන).
-     * කෝකියා මේ ගබඩාවයි, පොතයි තනියම හදාගන්නේ නැහැ. ආපනශාලාවේ මැනේජර් (ඒ කියන්නේ
-     * අපේ Spring Boot එක)
-     * කෝකියාව වැඩට බඳවා ගන්නකොටම, මේ ගබඩාවේ යතුරයි, පොතයි කෝකියාගේ අතට දෙනවා.
-     * "මෙන්න ඔයාට වැඩ කරන්න ඕනේ කරන දේවල් ටික" කියලා.
-     * 
-     * ඇත්තටම මේකෙන් කරන්නේ මොකක්ද? (Why do we do this?)
-     * අපේ Spring Boot program එක start වෙනකොට, එයා ඉබේම මේ TaskService එකක් හදනවා.
-     * එහෙම හදනකොට, Spring Boot විසින්ම මේ කියන TaskRepository එකකුත්,
-     * UserRepository එකකුත් හදලා මේ Constructor එක හරහා TaskService එක ඇතුළට දාලා
-     * දෙනවා.
-     * ඊට පස්සේ TaskService එක ඇතුළේ වෙනත් තැන් වලදී (උදාහරණයක් විදිහට අලුත් task
-     * එකක් database එකට save කරන්න වගේ ඕනෙම වෙලාවක), කිසිම ප්‍රශ්නයක් නැතුව අර
-     * Spring Boot එකෙන් දීපු Repository දෙක පාවිච්චි කරලා ඒ වැඩේ කරගන්න පුළුවන්.
-     * මේකට Programing වලදී කියන නම තමයි Dependency Injection. ඒ කියන්නේ අපිට ඕනේ
-     * කරන දේවල් (Dependencies) වෙනත් කෙනෙකුට කියලා (Spring Boot) අපේ ඇතුළට දාගන්න
-     * (Inject කරගන්න) එකයි.
-     */
-
-    public TaskService(TaskRepository taskRepository, UserRepository userRepository) {
-        this.taskRepository = taskRepository;
-        this.userRepository = userRepository;
-    }
-
-    public TaskResponseDTO createTask(TaskRequestDTO taskRequest) {
-        // Fetch the currently authenticated user from SecurityContextHolder.
-        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-
         /*
-         * Now we have know who is requesting to create a task, so we should create a
-         * task for that userId
-         * We will use that useremail to find that user in our database and fetch the
-         * user id So ,
-         * there will be no chance of creating a task for wrong userId If user is not
-         * found , then throw an exception.
-         * For that we will use ResourceNotFoundException. If user is found , then
-         * create a task for that user
+         * JAVA වලදී, int, boolean වගේ පොඩි දේවල් (Primitive types) කෙලින්ම variable
+         * එකක් ඇතුලේ තියාගන්න පුළුවන්
+         * වුණාට, TaskRepository වගේ ලොකු දේවල් (Objects) කෙලින්ම ඒක අස්සේ ඔබලා තියන්න
+         * බැහැ. ඒ වෙනුවට
+         * මේ variables කරන්නේ අර හැදුණු ඔරිජිනල් ගබඩාව තියෙන්නේ කොහෙද කියලා පෙන්වන එකයි
+         * (Refer කරන එකයි).
          */
-        UserEntity userObject = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found with email: " + userEmail));
+        private final TaskRepository taskRepository;
+        private final UserRepository userRepository;
+
+        // constructor (Dependency injection)
         /*
-         * Created an object from the TaskEntity.
-         * Now test the incoming taskRequest details to the newly created object.
+         * හිතන්න අපේ TaskService කියන්නේ ආපනශාලාවක ඉන්න ප්‍රධාන කෝකියා (Head Chef)
+         * කියලා.
+         * එයාගේ ප්‍රධාන වැඩේ තමයි ඔර්ඩර්ස් (Tasks) කළමනාකරණය කරන එක.
+         * හැබැයි කෝකියාට මේ වැඩේ තනියම කරන්න බැහැ. එයාට උදව්වට ප්‍රධාන දේවල් දෙකක් ඕනේ
+         * වෙනවා:
+         * TaskRepository - කෑම බඩු තියෙන ප්‍රධාන ගබඩාව (Database එකේ Tasks තියාගන්න
+         * තැන).
+         * UserRepository - පාරිභෝගිකයින්ගේ විස්තර තියෙන පොත (Database එකේ Users ලා ඉන්න
+         * තැන).
+         * කෝකියා මේ ගබඩාවයි, පොතයි තනියම හදාගන්නේ නැහැ. ආපනශාලාවේ මැනේජර් (ඒ කියන්නේ
+         * අපේ Spring Boot එක)
+         * කෝකියාව වැඩට බඳවා ගන්නකොටම, මේ ගබඩාවේ යතුරයි, පොතයි කෝකියාගේ අතට දෙනවා.
+         * "මෙන්න ඔයාට වැඩ කරන්න ඕනේ කරන දේවල් ටික" කියලා.
+         * 
+         * ඇත්තටම මේකෙන් කරන්නේ මොකක්ද? (Why do we do this?)
+         * අපේ Spring Boot program එක start වෙනකොට, එයා ඉබේම මේ TaskService එකක් හදනවා.
+         * එහෙම හදනකොට, Spring Boot විසින්ම මේ කියන TaskRepository එකකුත්,
+         * UserRepository එකකුත් හදලා මේ Constructor එක හරහා TaskService එක ඇතුළට දාලා
+         * දෙනවා.
+         * ඊට පස්සේ TaskService එක ඇතුළේ වෙනත් තැන් වලදී (උදාහරණයක් විදිහට අලුත් task
+         * එකක් database එකට save කරන්න වගේ ඕනෙම වෙලාවක), කිසිම ප්‍රශ්නයක් නැතුව අර
+         * Spring Boot එකෙන් දීපු Repository දෙක පාවිච්චි කරලා ඒ වැඩේ කරගන්න පුළුවන්.
+         * මේකට Programing වලදී කියන නම තමයි Dependency Injection. ඒ කියන්නේ අපිට ඕනේ
+         * කරන දේවල් (Dependencies) වෙනත් කෙනෙකුට කියලා (Spring Boot) අපේ ඇතුළට දාගන්න
+         * (Inject කරගන්න) එකයි.
          */
-        TaskEntity newTask = new TaskEntity();
-        newTask.setTitle(taskRequest.title());
-        newTask.setDescription(taskRequest.description());
-        newTask.setDueDate(taskRequest.dueDate());
-        newTask.setStatus("TODO");
-        newTask.setUser(userObject);
 
-        TaskEntity savedTask = taskRepository.save(newTask);
-
-        return new TaskResponseDTO(
-                savedTask.getId(),
-                savedTask.getTitle(),
-                savedTask.getDescription(),
-                savedTask.getStatus(),
-                savedTask.getDueDate());
-    }
-
-    public List<TaskResponseDTO> getUserTask() {
-
-        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserEntity userObject = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found with email: " + userEmail));
-        List<TaskEntity> userTasks = taskRepository.findAllByUser(userObject);
-
-        return userTasks.stream().map(task -> new TaskResponseDTO(
-                task.getId(),
-                task.getTitle(),
-                task.getDescription(),
-                task.getStatus(),
-                task.getDueDate())).toList();
-    }
-
-    public UpdateTaskDTO UpdateTask(Long taskId, UpdateTaskDTO updateRequest) {
-        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserEntity userObject = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found with email: " + userEmail));
-
-        TaskEntity existingTask = taskRepository.findById(taskId)
-                .orElseThrow(() -> new ResourceNotFoundException("No such task found!"));
-
-        if (!existingTask.getUser().getUserId().equals(userObject.getUserId())) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You don't have permission to eidt this task");
+        public TaskService(TaskRepository taskRepository, UserRepository userRepository) {
+                this.taskRepository = taskRepository;
+                this.userRepository = userRepository;
         }
 
-        existingTask.setTitle(updateRequest.title());
-        existingTask.setDescription(updateRequest.description());
-        existingTask.setDueDate(updateRequest.dueDate());
-        existingTask.setStatus(updateRequest.status());
+        public TaskResponseDTO createTask(TaskRequestDTO taskRequest) {
+                // Fetch the currently authenticated user from SecurityContextHolder.
+                String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        taskRepository.save(existingTask);
+                /*
+                 * Now we have know who is requesting to create a task, so we should create a
+                 * task for that userId
+                 * We will use that useremail to find that user in our database and fetch the
+                 * user id So ,
+                 * there will be no chance of creating a task for wrong userId If user is not
+                 * found , then throw an exception.
+                 * For that we will use ResourceNotFoundException. If user is found , then
+                 * create a task for that user
+                 */
+                UserEntity userObject = userRepository.findByEmail(userEmail)
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "User not found with email: " + userEmail));
+                /*
+                 * Created an object from the TaskEntity.
+                 * Now test the incoming taskRequest details to the newly created object.
+                 */
+                TaskEntity newTask = new TaskEntity();
+                newTask.setTitle(taskRequest.title());
+                newTask.setDescription(taskRequest.description());
+                newTask.setDueDate(taskRequest.dueDate());
+                newTask.setStatus("TODO");
+                newTask.setUser(userObject);
 
-        return new UpdateTaskDTO(
-                existingTask.getTitle(),
-                existingTask.getDescription(),
-                existingTask.getStatus(),
-                existingTask.getDueDate());
-    }
+                TaskEntity savedTask = taskRepository.save(newTask);
+
+                return new TaskResponseDTO(
+                                savedTask.getId(),
+                                savedTask.getTitle(),
+                                savedTask.getDescription(),
+                                savedTask.getStatus(),
+                                savedTask.getDueDate());
+        }
+
+        public List<TaskResponseDTO> getUserTask() {
+
+                String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+                UserEntity userObject = userRepository.findByEmail(userEmail)
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "User not found with email: " + userEmail));
+                List<TaskEntity> userTasks = taskRepository.findAllByUser(userObject);
+
+                return userTasks.stream().map(task -> new TaskResponseDTO(
+                                task.getId(),
+                                task.getTitle(),
+                                task.getDescription(),
+                                task.getStatus(),
+                                task.getDueDate())).toList();
+        }
+
+        public UpdateTaskDTO UpdateTask(Long taskId, UpdateTaskDTO updateRequest) {
+                String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+                UserEntity userObject = userRepository.findByEmail(userEmail)
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "User not found with email: " + userEmail));
+
+                TaskEntity existingTask = taskRepository.findById(taskId)
+                                .orElseThrow(() -> new ResourceNotFoundException("No such task found!"));
+
+                if (!existingTask.getUser().getUserId().equals(userObject.getUserId())) {
+                        throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                                        "You don't have permission to eidt this task");
+                }
+
+                existingTask.setTitle(updateRequest.title());
+                existingTask.setDescription(updateRequest.description());
+                existingTask.setDueDate(updateRequest.dueDate());
+                existingTask.setStatus(updateRequest.status());
+
+                taskRepository.save(existingTask);
+
+                return new UpdateTaskDTO(
+                                existingTask.getTitle(),
+                                existingTask.getDescription(),
+                                existingTask.getStatus(),
+                                existingTask.getDueDate());
+        }
+
+        public void deleteTask(Long taskId) {
+                String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+                UserEntity userObject = userRepository.findByEmail(userEmail)
+                        .orElseThrow(() -> new ResourceNotFoundException("User not found with email: " + userEmail));
+
+                TaskEntity existingTask = taskRepository.findById(taskId)
+                        .orElseThrow(() -> new ResourceNotFoundException("No such task found!"));
+
+                if (!existingTask.getUser().getUserId().equals(userObject.getUserId())) {
+                        throw new ResponseStatusException(HttpStatus.FORBIDDEN,"You don't have permission to Delete this task");
+                }
+
+                taskRepository.delete(existingTask);
+        }
 }
