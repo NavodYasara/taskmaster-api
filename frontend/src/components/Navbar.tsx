f3770a924004679d730c0c374c52331a2d471adf
Navod Yasara <navodyasara567@gmail.com>
2026-05-30 22:22:40 +0530

    feat: initialize frontend project with React Router and navigation structure

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -10,8 +10,12 @@ export default function Navbar() {
         </Typography>
         <Box>
           {/* We use React Router's <Link> instead of standard <a> tags so the page doesn't reload! */}
-          <Button color="inherit" component={Link} to="/login">Login</Button>
-          <Button color="inherit" component={Link} to="/register">Register</Button>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/register">
+            Register
+          </Button>
         </Box>
       </Toolbar>
     </AppBar>
