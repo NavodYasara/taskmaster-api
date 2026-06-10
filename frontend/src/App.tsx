bf966c414ea07be96f29a207139053c305922113
Navod Yasara <111131809+NavodYasara@users.noreply.github.com>
2026-06-10 06:22:07 +0530

    feat: implement Eisenhower Matrix UI with drag-and-drop task assignment except backend persistence (#4)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Register from "./pages/Register";
 import MyTasks from "./pages/MyTasks";
 import Login from "./pages/Login";
 import Matrix from "./pages/Matrix";
+import SandBoxPage from "./pages/SandBoxPage"
 
 const APP = () => {
   return (
@@ -19,6 +20,7 @@ const APP = () => {
             <Route element={<DashboardLayout />}>
               <Route path="/" element={<MyTasks />} />
               <Route path="/matrix" element={<Matrix />} />
+              <Route path="/sandbox" element={<SandBoxPage />} />
             </Route>
           </Routes>
         </div>
